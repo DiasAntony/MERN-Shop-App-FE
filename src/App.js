56fd2619c15f89f56a1ff2dafa6baac38b263e68
diaz <diazantony9997@example.com>
2023-05-10 23:05:20 +0530

    boots setup!!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
-
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
-
-  return(
-
-  <div>
-     <h1>hello</h1>
-    </div>
-  )
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+        <h1>hello</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
+
+// If we got responsive design problem don't worry we would change the version of bootstrap 4.5.2 and bootstrap.min.css!!
