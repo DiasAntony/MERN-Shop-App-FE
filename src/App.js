43ab1f8d92de1a68acbf04caf0ec5ccf984a7b8b
diaz <diazantony9997@example.com>
2023-05-13 21:44:32 +0530

    shipping and payment

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import Shipping from "./pages/Shipping";
+import Payment from "./pages/Payment";
 
 function App() {
   return (
@@ -18,6 +23,11 @@ function App() {
             <Route path="/product/:id" Component={ProductDetails} />
             {/* question(?) mark give a different logic if it not that route not work=> when use ----  /cart  ----- */}
             <Route path="/cart/:id?" Component={Cart} />
+            <Route path="/login" Component={Login} />
+            <Route path="/register" Component={Register} />
+            <Route path="/profile" Component={Profile} />
+            <Route path="/shipping" Component={Shipping} />
+            <Route path="/payment" Component={Payment} />
           </Routes>
         </Container>
       </main>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -31,7 +31,8 @@ const CartScreen = () => {
   }
 
   const checkoutHandler = () => {
-    navigate('/login?redirect=shipping')
+    navigate('/shipping')
+    // navigate('/login?redirect=shipping')
   }
 
   return (
